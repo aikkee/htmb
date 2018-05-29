6d3109f09b960d5d9ec930d1fbfe9af69bd8c2d5
aikkee <aikkee@gmail.com>
2018-05-29 10:34:58 +0800

    Resync with correct local repo

--- wsgi.py
+++ wsgi.py
@@ -19,27 +19,29 @@ db = SQLAlchemy(application)
 class BookingForm(Form):
     location = SelectField('location')
     date_time = SelectField('date_time')
+    rtype = HiddenField('rtype')
     booking_ref = HiddenField('booking_ref')
     expire_on = HiddenField('expire_on')
 
 # -- Models --
-class Triage(db.Model):
+class Resource(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    rtype = db.Column(db.String(10)) # Resource Type
     location = db.Column(db.String(10))
     description = db.Column(db.String(20))
     capacity = db.Column(db.Integer)
     available = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Triage %r, %r, %r, %r>' % (self.location, self.description, \
+        return '<Resource %r, %r, %r, %r, %r>' % (self.rtype, self.location, self.description, \
             self.capacity, self.available)
 
 class Reference(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    case_id = db.Column(db.String(10), unique=True) # Cross reference back to EITP in future
-    booking_ref = db.Column(db.String(10), unique=True) # Randomised id that is not in sequence like case_id
+    resource_type = db.Column(db.String(10), unique=True) # Resources that this reference can book
+    booking_ref = db.Column(db.String(10), unique=True) # reference made to the person who make a booking
     expire_on = db.Column(db.DateTime)
-    triage_id = db.Column(db.Integer, db.ForeignKey('triage.id'))
+    resource_id = db.Column(db.Integer, db.ForeignKey('resource.id'))
     update_on = db.Column(db.DateTime)
 
     def __repr__(self):
@@ -57,11 +59,12 @@ def index():
 def init():
     ref = request.args.get('ref')
     isnew = request.args.get('isnew')
+    rtype = request.args.get('rtype')
     # also use this as the indicator for where the request comes from
     if isnew is None or isnew=='':
         isnew='yes'
 
-    if (isValidReference(ref) == False):
+    if (isValidReference(ref, rtype) == False):
         return redirect(url_for('index'))
 
     if ref is None or ref == '':
@@ -69,32 +72,33 @@ def init():
 
     # TODO: Improve the query later by combining the 2 statements
     reference = Reference.query.filter(Reference.booking_ref==ref).first()
-    booked_triage = Triage.query.filter(Triage.id==reference.triage_id).first()
+    booked_resource = Resource.query.filter(Resource.id==reference.resource_id).first()
     # Need to also handle cases where user use the same link to amend booking
     # When initiated from user through the sms link, isnew will be 'yes'
     # When initiated from change_booking page, isnew will be 'no'
-    if isnew=='yes' and booked_triage!=None:
-        m = {'location':booked_triage.location, \
-              'date_time':booked_triage.description}
-        b = [{'ref':ref, 'isnew':'no', 'label':'Yes'}]
+    if isnew=='yes' and booked_resource!=None:
+        m = {'location':booked_resource.location, \
+              'date_time':booked_resource.description}
+        b = [{'ref':ref, 'rtype':rtype, 'isnew':'no', 'label':'Yes'}]
         return render_template('change_booking.html', message=m, buttons=b)
 
-    # obtain the triage slots from database
-    # available_triages = Triage.query.filter(Triage.available>0)
+    # obtain the resource slots from database
+    # available_resource = Resource.query.filter(Resource.available>0)
     bookingForm = BookingForm()
-    # bookingForm.date_time.choices = [('', 'Choose Date & Time')] + [ (t.id, t.description) for t in available_triages]
+    # bookingForm.date_time.choices = [('', 'Choose Date & Time')] + [ (t.id, t.description) for t in available_resource]
     bookingForm.date_time.choices = [('', 'Choose Date & Time')]
 
     # obtain locations of available slots
-    triages = Triage.query.filter(Triage.available>0)
-    locations = list(set([(t.location) for t in triages if show_for_booking(t, str(reference.expire_on))]))
-    print 'Printing locations'
-    for l in locations:
-        print l
+    resources = Resource.query.filter(and_(Resource.available>0, Resource.rtype==rtype)).all()
+
+    print('Resource count for reference: %r' % len(resources))
+
+    locations = list(set([(r.location) for r in resources if show_for_booking(r, str(reference.expire_on), rtype)]))
     bookingForm.location.choices = [('', 'Choose Location')] + [
         (l, l) for l in locations]
     
     bookingForm.booking_ref.data = ref
+    bookingForm.rtype.data = rtype
     if reference != None:
         bookingForm.expire_on.data = reference.expire_on
     return render_template('book_htmb_form.html', form=bookingForm, isnew='yes')
@@ -107,35 +111,36 @@ def save():
         return redirect(redirect_url())
 
     b_ref = request.form.get('booking_ref')
-    tid = request.form.get('date_time', type=int)
+    r_id = request.form.get('date_time', type=int)
 
     reference = Reference.query.filter(Reference.booking_ref==b_ref).first()
     # Release booking slot
-    if reference.triage_id is not None:
-        booked_triage = Triage.query.filter(Triage.id==reference.triage_id).first()
-        if booked_triage is not None:
-            booked_triage.available = booked_triage.available + 1 
-            application.logger.info('Return slot for Triage ID: %r' % booked_triage.id)
+    if reference.resource_id is not None:
+        booked_resource = Resource.query.filter(Resource.id==reference.resource_id).first()
+        if booked_resource is not None:
+            booked_resource.available = booked_resource.available + 1 
+            application.logger.info('Return slot for Resource ID: %r' % booked_resource.id)
             db.session.commit()
 
-    triage = Triage.query.filter(Triage.id==tid).first()
-    if triage.available == 0:
+    resource = Resource.query.filter(Resource.id==r_id).first()
+    if resource.available == 0:
         # Fully booked, cannot accept anymore
-        m = {'location':triage.location, \
-              'date_time':triage.description}
+        m = {'location':resource.location, \
+              'date_time':resource.description}
         b = [{'ref':b_ref, 'label':'OK'}]
         return render_template('fully_booked.html', message=m, buttons=b)
     else:
         # update new booking
-        reference.triage_id = tid
+        reference.resource_id = r_id
         reference.update_on = datetime.datetime.now()
             
-        triage.available = triage.available - 1 
+        resource.available = resource.available - 1 
 
-        application.logger.info('Draw down from Triage ID: %s for Reference ID: %s' % (tid, reference.id))
+        application.logger.info('Draw down from Resource ID: %s for Reference ID: %s' % (r_id, reference.id))
 
         db.session.commit()
-        flash('Thank you for your submission. You will receive our SMS confirmation soon.')
+        flash('Thank you for your submission.  Your session on %s at %s is confirmed.' %
+                (resource.description, resource.location))
         return render_template('acknowledge.html')
 
 
@@ -144,33 +149,38 @@ def redirect_url(default='index'):
            request.referrer or \
            url_for(default)
 
-def show_for_booking(triage, exp, location=None):
+# Function to filter out and handle expiry date
+# rtype determines the type of resources to be retreived, hence it should not be None
+def show_for_booking(resource, exp, rtype, location=None):
     # handle reference expiry date
 
     exp_date = date( int(exp[:4]), int(exp[5:7]), int(exp[8:10]) )
-    earliest_date = exp_date + datetime.timedelta(days=5)
-    # change triage description to date object
-    triage_date = datetime.datetime.strptime(triage.description[:11], '%d %b %Y').date()
+    earliest_date = exp_date + datetime.timedelta(days=3)
+    # change resource description to date object
+    resource_date = datetime.datetime.strptime(resource.description[:11], '%d %b %Y').date()
     if location == None:
-        return triage.available > 0 and triage_date > earliest_date
+        return resource.available > 0 and resource.rtype == rtype and resource_date > earliest_date
     else:
-        return triage.location == location and triage.available > 0 and triage_date > earliest_date
+        return resource.location == location and resource.rtype == rtype and resource.available > 0 and resource_date > earliest_date
          
 
+#TODO
 @application.route("/slotsfor/<location>/", methods=["GET", "POST"])
 def get_slots(location):
-    triages = Triage.query.all()
+    resources = Resource.query.all()
     ref_expiry = request.args['expire_on']
+    rtype = request.args['rtype']
     data = [
-        (t.id, t.description) for t in triages if show_for_booking(t, ref_expiry, location) 
+        (r.id, r.description) for r in resources if show_for_booking(r, ref_expiry, rtype, location) 
     ]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
 
 
-def isValidReference(booking_ref):
+def isValidReference(booking_ref, rtype):
     bRef = Reference.query.filter(and_(Reference.booking_ref==booking_ref,
+                                       Reference.resource_type==rtype,
                                        Reference.expire_on>datetime.datetime.now())).first()
     if (bRef != None):
         return True
@@ -180,24 +190,25 @@ def isValidReference(booking_ref):
 
 @application.route('/admin/check_slots')
 def check_slots():
-    return render_template('show_slots.html', slots=Triage.query.all())
+    return render_template('show_slots.html', slots=Resource.query.all())
 
 @application.route("/admin/add_case", methods=['GET', 'POST'])
 def add_case():
     # handle submission
     if request.method == 'POST':
+        rtype = request.form.get('rtype')
         ref = request.form.get('ref')
         expiry = request.form.get('expiry')
-        r = create_new_reference(ref, expiry)
+        r = create_new_reference(rtype, ref, expiry)
         flash('%s activated. Expires on %s.' \
                 % (r.booking_ref, r.expire_on.strftime('%d-%b-%Y')))
         return render_template('add_case.html')
 
     return render_template('add_case.html')
 
-def create_new_reference(ref, expiry):
+def create_new_reference(rtype, ref, expiry):
     r = Reference()
-    r.case_id = 'ADMIN'
+    r.resource_type = rtype
     r.booking_ref = ref 
     expiry_str = expiry + " 16:00:00" #handle timezone of server - 8 hrs behind us
     r.expire_on = datetime.datetime.strptime(expiry_str, "%Y-%m-%d %H:%M:%S")
@@ -210,7 +221,7 @@ def import_case():
     if request.method == 'POST':
         def ref_init_func(row):
             r = Reference()
-            r.case_id = row['CASE_ID']
+            r.resource_type = row['RESOURCE_TYPE']
             r.booking_ref = row['BOOKING_REF']
             r.expire_on = datetime.datetime.strptime(row['EXPIRE_ON'], \
                 "%Y-%m-%d %H:%M:%S")
@@ -225,16 +236,17 @@ def import_case():
 @application.route("/admin/import_slot", methods=['GET', 'POST'])
 def import_slot():
     if request.method == 'POST':
-        def triage_init_func(row):
-            t = Triage()
-            t.location = row['LOCATION']
-            t.description = row['DESCRIPTION']
-            t.capacity = row['CAPACITY']
-            t.available = row['CAPACITY']
-            return t
+        def resource_init_func(row):
+            r = Resource()
+            r.rtype = row['TYPE']
+            r.location = row['LOCATION']
+            r.description = row['DESCRIPTION']
+            r.capacity = row['CAPACITY']
+            r.available = row['CAPACITY']
+            return r
         request.save_book_to_database(field_name='file', session=db.session, \
-                                      tables=[Triage], \
-                                      initializers=([triage_init_func]))
+                                      tables=[Resource], \
+                                      initializers=([resource_init_func]))
         flash('HTMB timeslot created')
         return render_template('acknowledge.html')
     return render_template('import.html', title='Import HTMB Timeslot')
@@ -242,12 +254,12 @@ def import_slot():
 @application.route("/admin/export_booking", methods=['GET'])
 def doexport():
 
-    col = ['case_id', 'booking_ref','description', 'update_on', 'location']
-    qs = Reference.query.join(Triage, Reference.triage_id == Triage.id).add_columns( \
-            Reference.case_id, Reference.booking_ref, Triage.description, \
-            Reference.update_on, Triage.location).filter( \
-                Reference.triage_id != None)
-    # qs = Triage.query.filter(Triage.booking_ref != None)
+    col = ['resource_type', 'booking_ref','description', 'update_on', 'location']
+    qs = Reference.query.join(Resource, Reference.resource_id == Resource.id).add_columns( \
+            Reference.resource_type, Reference.booking_ref, Resource.description, \
+            Reference.update_on, Resource.location).filter( \
+                Reference.resource_id != None)
+    # qs = Resource.query.filter(Resource.booking_ref != None)
     return excel.make_response_from_query_sets(qs, col, "csv")
 
 @application.errorhandler(404)
