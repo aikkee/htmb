e90060d0e0feac432fe85e38a4b22ddbfebe283a
aikkee <aikkee@gmail.com>
2019-05-15 03:56:10 +0800

    Add address for medical center

--- static/bookings.js
+++ static/bookings.js
@@ -1,32 +1,31 @@
-$(function() {
+var addrLookup = {
+  "SCDF HQ":"91 Ubi Ave 4 S(408827)",
+  "CDA":"101 Jalan Bahar S(649734)",
+  "HTA":"501 Old Choa Chu Kang Rd S(698928)"}
+function getAddress(k){
+  return addrLookup[k];
+}
 
-  // test to ensure jQuery is working
-  console.log("whee!")
 
-  // disable refresh button
-  // $("#refresh-btn").prop("disabled", true)
-  
+//$("#location_select").change(function() {
+$( document ).on("change", "#location_select", function() {
 
+  // grab value
+  var location_id = $("#location_select").val();
+  	var dataString = $("#htmbForm").serialize();
 
-  //$("#location_select").change(function() {
-  $( document ).on("change", "#location_select", function() {
+  // send value via POST to URL /<department_id>
+  var get_request = $.ajax({
+    type: 'GET',
+  		data: dataString,
+    url: '/slotsfor/' + location_id + '/'
+  });
 
-    // grab value
-    var location_id = $("#location_select").val();
-		var dataString = $("#htmbForm").serialize();
+  // handle response
+  get_request.done(function(data){
 
-    // send value via POST to URL /<department_id>
-    var get_request = $.ajax({
-      type: 'GET',
-			data: dataString,
-      url: '/slotsfor/' + location_id + '/'
-    });
-
-    // handle response
-    get_request.done(function(data){
-
-      // data
-      console.log(data)
+    // data
+    console.log(data)
 
       // add values to list 
       var option_list = [["", "Choose Date & Time"]].concat(data);
@@ -38,5 +37,6 @@ $(function() {
       // show model list
       $("#slot_select").show();
     });
+    //show address
+    $('.addressText').text(getAddress(location_id));
   });
-})
